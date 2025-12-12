cf8fdc156422690452027389368c4c8c28ee5036
Mathis Poncet <math712b@gmail.com>
2025-12-12 12:32:01 +0100

    fix(ts types): declare 2 forgotten custom expect matchers ts definition

--- src/test-expect.ts
+++ src/test-expect.ts
@@ -11,6 +11,19 @@ interface CustomMatchers<R = unknown> {
   toHaveReceivedEventDetail(eventDetail: any): R;
 
   /**
+   * Will check if the event spy received the expected event at the given index with the expected detail.
+   * @param index position of the event in the received events array.
+   * @param eventDetail The expected detail of the event.
+   */
+  toHaveNthReceivedEventDetail(index: number, eventDetail: any): R;
+
+  /**
+   * Will check if the event spy received the expected event with the expected detail on the first received event.
+   * @param eventDetail The expected detail of the event.
+   */
+  toHaveFirstReceivedEventDetail(eventDetail: any): R;
+
+  /**
    * Will check how many times the event has been received.
    */
   toHaveReceivedEventTimes(count: number): R;
